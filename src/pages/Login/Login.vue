a2524b5c678c8b94418233d7e358bd5030fecd5c
洋 <610762935@qq.com>
2020-01-07 09:20:16 +0800

    Signed-off-by: 洋 <610762935@qq.com>

---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="a">
+            123
+            <div class="b">456</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+
+    ],
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+        this.getadata()
+    },
+    watch: {
+
+    },
+    methods: {
+        getadata(){
+            this.$axios("https://api.zbztb.cn/api/public/v1/home/swiperdata")
+            .then(res=>{
+                console.log(res);
+                
+            })
+        }
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="less">
+.a{
+    color: red;
+    .b{
+        color:green
+    }
+}
+</style>
